168daa59b5df8595f3690e88659324ee091a40e5
Thalles Batista <thallesaugusto.mb@gmail.com>
2025-08-06 18:17:02 -0300

    Feat: Add DTOs

---
+++ DTO/Produto/ProdutoEdicaoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiArmazem.DTO.Produto
+{
+    public class ProdutoEdicaoModel
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public int QuantidadeEstoque { get; set; }
+        public string CodigoDeBarras { get; set; } = string.Empty;
+        public string Marca { get; set; } = string.Empty;
+    }
+}
